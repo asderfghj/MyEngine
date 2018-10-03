c8d04c8263059b445b2e52f712420551ff40d8e0
Martin Dean (i7464614) <i7464614@bournemouth.ac.uk>
2018-10-03 13:00:00 +0100

    added variables and functions to core class in engine library

---
+++ src/myengine/Core.cpp
@@ -0,0 +1,26 @@
+#include "Core.h"
+
+std::shared_ptr<Core> Core::Init()
+{
+	
+	//put other setup stuff here
+	return std::shared_ptr<Core>(this);
+}
+
+void Core::Start()
+{
+	_running = true;
+}
+
+void Core::Stop()
+{
+	_running = false;
+}
+
+std::shared_ptr<Entity> Core::addEntity()
+{
+	std::shared_ptr <Entity> newEntity(new Entity());
+	_entities.push_back(newEntity);
+	return newEntity;
+}
+
--- src/myengine/Core.h
+++ src/myengine/Core.h
@@ -1,4 +1,26 @@
+#include <memory>
+#include <vector>
+
+class Environment
+{};
+
+class Entity
+{};
+
+class Input
+{};
+
 class Core
 {
-	
+private:
+	std::shared_ptr<Environment> _environment;
+	std::vector<std::shared_ptr<Entity>> _entities;
+	std::shared_ptr<Input> _input;
+	bool _running;
+
+public:
+	std::shared_ptr<Core> Init();
+	void Start();
+	void Stop();
+	std::shared_ptr<Entity> addEntity();
 };
